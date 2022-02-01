3e1b86ac7e65ce4ea83f9f0b23586be46a7864ab
Pierre Mulliez <37341575+Pierre-mulliez1@users.noreply.github.com>
2022-02-01 11:24:48 +0100

    Following Dev local

--- Dev/form_helper.js
+++ Dev/form_helper.js
@@ -4,6 +4,25 @@ document.write(`
 <div class = "menutop">
 <a href = "index.html"  style = 'padding: 4px 6px'> <img class = "container_img" src="pictures/logo2.png" alt="logo" > </a>
 <a href = "insert_rest.html" style = 'float:right;' class = "container_text">Your restaurant </a>
+<button style = 'float:left;' class = "container_text" onclick="myPoplog();">User Log in </button>
+</div>
+
+<div class = "popup" id="windowpop"  style="margin-left: 20%;width:60%" >
+<span class="popuptext_logs" id="myPoplog">
+    <form action="" method="">
+        <h5>Your username:</h5> <input type="text" name = "IA_name">
+        <h5>Your password:</h5> <input type="text" id= "IA_process">
+     </form>
+     <button onclick="">Sign in</button>
+     <button onclick="myPop00()">cancel </button>
+</span>
+<script>function myPoplog() {
+    var popup = document.getElementById("myPoplog");
+    if ( popup.classList.toggle === 'hide'){
+    popup.classList.toggle("show");}
+    else { popup.classList.toggle("show");}
+  }
+  </script>
 </div>
 `);
 
